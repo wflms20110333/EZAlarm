ba74be0ebaafb6ab7a30657be8336293233e3b51
liwanlin41 <liwanlin@mit.edu>
2018-09-15 17:24:41 -0400

    Hi

---
+++ app/src/main/java/com/example/elizabethzou/hackmit/Place.java
@@ -0,0 +1,9 @@
+package com.example.elizabethzou.hackmit;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+
+public class Place{
+    public static final String NETWORK_PROVIDER;
+    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+}
