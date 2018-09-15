103d1f4ceb54016f6a2372bdd6629a8c3ac740f4
liwanlin41 <liwanlin@mit.edu>
2018-09-15 18:03:07 -0400

    Hi

--- app/src/main/java/com/example/elizabethzou/hackmit/Place.java
+++ app/src/main/java/com/example/elizabethzou/hackmit/Place.java
@@ -33,4 +33,20 @@ public class Place{
             startActivity(settingsIntent);
         }
     }
+
+    private final LocationListener listener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // A new location update is received.  Do something useful with it.  In this case,
+            // we're sending the update to a handler which then updates the UI with the new
+            // location.
+            Message.obtain(mHandler,
+                    UPDATE_LATLNG,
+                    location.getLatitude() + ", " +
+                            location.getLongitude()).sendToTarget();
+        }
+    };
+
+    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000000, 100, listener);
 }
