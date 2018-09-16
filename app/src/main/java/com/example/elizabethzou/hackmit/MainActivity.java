f89a0f553e532a5a48855923eb19e624a9a54314
Elizabeth Zou <elizabeth.zou.0@gmail.com>
2018-09-15 23:05:11 -0400

    more calendar stuff

--- app/src/main/java/com/example/elizabethzou/hackmit/MainActivity.java
+++ app/src/main/java/com/example/elizabethzou/hackmit/MainActivity.java
@@ -14,10 +14,10 @@ import android.text.format.Time;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -25,66 +25,64 @@ import static android.provider.AlarmClock.ACTION_SET_ALARM;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
-    Cursor cursor;
+    public static long MILLISECONDS_PER_DAY = 86400000L;
     Button button2; //button for alarm
     Long time = null; //time for earliest event
     Long prelay = new Long("18000000"); //default morning routine time
-    List<CalEvent> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        findViewById(R.id.button).setOnClickListener(this);
         button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(this);
         //erase existing
+    }
+
+    public CalEvent getEarliestEvent(Calendar day)
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED)
+            return null;
+
+        List<CalEvent> events = new ArrayList<>();
+        long start = day.getTimeInMillis();
+        long end = start + MILLISECONDS_PER_DAY;
+        long currDay = Time.getJulianDay(start, -14400);
+        Uri myUri = Uri.parse("content://com.android.calendar/instances/when/" + start + "/" + end);
+        Cursor instancesCursor = getContentResolver().query(myUri, null, null, null, null);
+        while (instancesCursor.moveToNext()) {
+            if (instancesCursor != null) {
+                int id_1 = instancesCursor.getColumnIndex(CalendarContract.Instances.TITLE);
+                int id_2 = instancesCursor.getColumnIndex(CalendarContract.Instances.START_MINUTE);
+                int id_3 = instancesCursor.getColumnIndex(CalendarContract.Instances.START_DAY);
+                int id_4 = instancesCursor.getColumnIndex(CalendarContract.Instances.END_MINUTE);
+                int id_5 = instancesCursor.getColumnIndex(CalendarContract.Instances.EVENT_LOCATION);
+                String title = instancesCursor.getString(id_1);
+                int startMinute = Integer.parseInt(instancesCursor.getString(id_2));
+                long startDay = Long.parseLong(instancesCursor.getString(id_3));
+                int endMinute = Integer.parseInt(instancesCursor.getString(id_4));
+                String location = instancesCursor.getString(id_5);
+                if (startDay != currDay || startMinute <= 0 && endMinute >= 1440)
+                    continue;
+                events.add(new CalEvent(title, startMinute, location));
+            }
+        }
+        Collections.sort(events);
+        Log.i("rip", "lol " + events);
+        return events.isEmpty() ? null: events.get(0);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-                    //Log.i("didnotpasstest", "rip");
-                    return;
-                }
-                //Log.i("passedTest", "hey");
-                events = new ArrayList<>();
-
                 Calendar c = Calendar.getInstance();
                 c.set(Calendar.HOUR_OF_DAY, 0);
                 c.set(Calendar.MINUTE, 0);
                 c.set(Calendar.SECOND, 0);
                 c.set(Calendar.MILLISECOND, 0);
-                long start = c.getTimeInMillis();
-                c.add(Calendar.DAY_OF_MONTH, 1);
-                long end = c.getTimeInMillis();
-                long day = Time.getJulianDay(System.currentTimeMillis(), -14400);
-                Uri myUri = Uri.parse("content://com.android.calendar/instances/when/" + start + "/" + end);
-                Cursor instancesCursor = getContentResolver().query(myUri, null, null, null, null);
-                while (instancesCursor.moveToNext()) {
-                    if (instancesCursor != null) {
-                        int id_1 = instancesCursor.getColumnIndex(CalendarContract.Instances.TITLE);
-                        int id_2 = instancesCursor.getColumnIndex(CalendarContract.Instances.START_MINUTE);
-                        int id_3 = instancesCursor.getColumnIndex(CalendarContract.Instances.START_DAY);
-                        int id_4 = instancesCursor.getColumnIndex(CalendarContract.Instances.END_MINUTE);
-                        int id_5 = instancesCursor.getColumnIndex(CalendarContract.Instances.EVENT_LOCATION);
-                        String title = instancesCursor.getString(id_1);
-                        int startMinute = Integer.parseInt(instancesCursor.getString(id_2));
-                        long startDay = Long.parseLong(instancesCursor.getString(id_3));
-                        int endMinute = Integer.parseInt(instancesCursor.getString(id_4));
-                        String location = instancesCursor.getString(id_5);
-                        if (startDay != day || startMinute <= 0 && endMinute >= 1440)
-                            continue;
-                        events.add(new CalEvent(title, startMinute, location));
-                    } else {
-                        Toast.makeText(this, "Event is not present.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                Log.i("rip", "lol " + events);
+                getEarliestEvent(c);
                 break;
 
             case R.id.button2:
@@ -113,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public static class CalEvent
+    public static class CalEvent implements Comparable<CalEvent>
     {
         String title;
         int start;
@@ -126,9 +124,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             location = c;
         }
 
+        @Override
         public String toString()
         {
             return title + ": " + start + ", " + location;
+        }
+
+        @Override
+        public int compareTo(CalEvent o)
+        {
+            return this.start - o.start;
         }
     }
 }
