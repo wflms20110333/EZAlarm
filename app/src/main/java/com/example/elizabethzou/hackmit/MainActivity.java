41f5737ace57aad5a3a6da576fab6dc528d0f3bb
Shengtong Zhang <stzh1555@mit.edu>
2018-09-16 01:03:21 -0400

    should be working

--- app/src/main/java/com/example/elizabethzou/hackmit/MainActivity.java
+++ app/src/main/java/com/example/elizabethzou/hackmit/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.AlarmClock;
 import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
@@ -27,8 +28,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 import static android.provider.AlarmClock.ACTION_DISMISS_ALARM;
 import static android.provider.AlarmClock.ACTION_SET_ALARM;
+import static java.lang.Thread.sleep;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -40,12 +44,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button2; //button for alarm
     Long time = null; //time for earliest event
     Long prelay = new Long("18000000"); //default morning routine time
-    List<CalEvent> events;
-    String filename = "Elizabeth_bot.txt";
+    String filename = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS)+"/Elizabeth_bot.txt";
     File file = new File(filename);
     OutputStreamWriter osw;
     InputStreamReader isr;  //file stuff
-    Record[] records = new Record[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,35 +63,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Boolean bool = file.createNewFile();
                 if (!bool) throw new Exception("File Creation Fail");
             }
-            FileOutputStream fOut = openFileOutput(filename, MODE_PRIVATE);
-            osw = new OutputStreamWriter(fOut);
-            //TODO: remove set alarm;
         }
         catch (Exception e)  {
             //TODO: push appropriate alarm message;
-            break;
+            Log.i("Error", e.getMessage());
         }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button:
+            case R.id.button: {
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
                     Log.i("didnotpasstest", "rip");
                     return;
                 }
                 Log.i("passedTest", "hey");
-                events = new ArrayList<>();
                 cursor = getContentResolver().query(Uri.parse("content://com.android.calendar/calendars"), null, null, null, null);
-                //cursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, null, null, null, null);
+                cursor = getContentResolver().query(CalendarContract.Events.CONTENT_URI, null, null, null, null);
                 while (cursor.moveToNext()) {
                     if (cursor != null) {
-                        int id_1 = cursor.getColumnIndex(CalendarContract.Instances.TITLE);
-                        int id_2 = cursor.getColumnIndex(CalendarContract.Instances.BEGIN);
-                        String title = cursor.getString(id_1);
-                        Date start = new Date(Long.parseLong(cursor.getString(id_2)));
-                        events.add(new CalEvent(title, start));
+                        //int id_1 = cursor.getColumnIndex(CalendarContract.Instances.TITLE);
+                        //int id_2 = cursor.getColumnIndex(CalendarContract.Instances.BEGIN);
+                        //String title = cursor.getString(id_1);
+                        //Date start = new Date(Long.parseLong(cursor.getString(id_2)));
+                        //events.add(new CalEvent(title, start));
 
                         //int id_1 = cursor.getColumnIndex(CalendarContract.Events._ID);
                         int id_2 = cursor.getColumnIndex(CalendarContract.Events.TITLE);
@@ -99,29 +97,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         int id_6 = cursor.getColumnIndex(CalendarContract.Events.DTEND);
                         //String idValue = cursor.getColumnName(id_1);
                         String titleValue = cursor.getString(id_2);
-                        String descriptionValue = cursor.getString(id_3);
-                        String eventValue = cursor.getString(id_4);
+                        //String descriptionValue = cursor.getString(id_3);
+                        //String eventValue = cursor.getString(id_4);
                         String startValue = cursor.getString(id_5);
                         time = calculateTime(Long.parseLong(startValue));
                         String endValue = cursor.getString(id_6);
                         Toast.makeText(this, startValue, Toast.LENGTH_SHORT).show();
-                        setAlarm(titleValue, time);
+                        makeAlarm(titleValue, time);
                         //String descriptionValue = cursor.getString(id_3);
                         //String eventValue = cursor.getString(id_4);
-                        Date startValue = new Date(Long.parseLong(cursor.getString(id_5)));
-                        Date endValue = new Date(Long.parseLong(cursor.getString(id_6)));
-                        events.add(new CalEvent(titleValue, startValue, endValue));
+                        //Date startValue = new Date(Long.parseLong(cursor.getString(id_5)));
+                        //Date endValue = new Date(Long.parseLong(cursor.getString(id_6)));
+                        //events.add(new CalEvent(titleValue, startValue, endValue));
                         Toast.makeText(this, titleValue + ", " + startValue + ", " + endValue, Toast.LENGTH_SHORT).show();
                         //Log.i("title", "lol " + titleValue);
                         //Log.i("dtstart", "lol " + startValue);
                         //Log.i("dtend", "lol " + endValue);
-
+                        break;
                     } else {
                         Toast.makeText(this, "Event is not present.", Toast.LENGTH_SHORT).show();
                     }
                 }
-                Log.i("rip", "lol " + events);
-                break;
+            }
         }
     }
     //-------------------------FILE COMPONENTS--------------------------
@@ -129,8 +126,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         String title;
         Long millis;
-        String valid;
-        public Record(String title, Long millis) {
+        public Record(String title, Long millis)
+        {
             this.title = title.replaceAll(" ", "_"); //erase spaces to avoid trouble
             this.millis = millis;
         }
@@ -154,35 +151,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //TODO: push error message
         }
     }
-    public Long eraseAll()
+    Long toKill;
+    public void killAll()
     {
         try
         {
-            FileInputStream fIn = openFileInput(filename);
+            FileInputStream fIn = new FileInputStream(file);
             isr = new InputStreamReader(fIn);
             BufferedReader in = new BufferedReader(isr);
             String str;
-            while ((str = in.readLine()) != null)  //read until EOF
+            Log.i("Erasing begins","");
+            Integer count = 0;
+            while ((str = in.readLine()) != null && str.length() != 0)  //read until EOF
             {
+                Log.i("to be erased", str);
                 String[] str1 = str.split(" ");
-                eraseAlarm(str1[1]);
+                toKill = Long.parseLong(str1[1]);
+                killAlarm(Long.parseLong(str1[1]));
+                count++;
             }
+            Log.i("Erasing ends",count.toString());
+            in.close();
         }
         catch (Exception e)
         {
-            //TODO: push error message
-        } 
+            Log.i("Error",e.getMessage());//TODO: push error message
+        }
     }
     //API for Elizabeth
     public Long retrieveTime(String title)  {
         try
         {
-            String goal = title.replace(" ","_")
+            String goal = title.replace(" ","_");
             FileInputStream fIn = openFileInput(filename);
             isr = new InputStreamReader(fIn);
             BufferedReader in = new BufferedReader(isr);
             String str;
-            while ((str = in.readLine()) != null)  //read until EOF
+            while ((str = in.readLine()) != null && str.length() != 0)  //read until EOF
             {
                  String[] str1 = str.split(" ");
                  if (str1[0].equals(goal))
@@ -190,11 +195,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                      return Long.parseLong(str1[1]);
                  }
             }
+            return null;
         }
         catch (Exception e)
         {
             //TODO: push error message
         }
+        return null;
     }
     //-------------------------ALARM COMPONENTS-------------------------
     //calculate time to set alarm
@@ -203,33 +210,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return time - prelay; //TODO: discretion
     }
 
-    public void eraseAlarm(String title)
+    public void killAlarm(String title)
     {
-        eraseAlarm(retrieveTime(title));
+        killAlarm(retrieveTime(title));
     }
     //erase alarm given time
-    public void eraseAlarm(Long millis)  {
+    Intent eraseAlarm;
+
+    public void killAlarm(Long millis)  {
         Boolean permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM) != PackageManager.PERMISSION_GRANTED;
         Boolean calenderSet = time == null;
         if (permission || calenderSet) {
             Log.i("didnotpasstest", "rip");
             return;
         }
-        Intent eraseAlarm = new Intent();
-        Calendar calendar = new GregorianCalendar();
-        calendar.setTimeInMillis(millis);
-        eraseAlarm.setAction(ACTION_DISMISS_ALARM);
-        eraseAlarm.putExtra(AlarmClock.EXTRA_HOUR, calendar.HOUR);
-        eraseAlarm.putExtra(AlarmClock.EXTRA_MINUTES, calendar.MINUTE);
-        eraseAlarm.putExtra(AlarmClock.EXTRA_IS_PM, false);
-        startActivity(eraseAlarm);
+
+        //postponed
     }
 
 
     //setAlarm
-    public void setAlarm(String title, Long millis)
+    public void makeAlarm(String title, Long millis)
     {
-        eraseAll();
+        killAll();
+
         Boolean permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.SET_ALARM) != PackageManager.PERMISSION_GRANTED;
         Boolean calenderSet = time == null;
         if (permission || calenderSet) {
@@ -250,43 +254,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setAlarm.putExtra(AlarmClock.EXTRA_MINUTES, calendar.MINUTE);
         //in the morning;
         setAlarm.putExtra(AlarmClock.EXTRA_IS_PM, false);
-        startActivity(setAlarm);
-        //put record
         Record rc = new Record(title, millis);
+        startActivity(setAlarm);
+
+        if (!toKill.equals(millis))
+        {
+            Log.i("???", ((Long)(millis - toKill)).toString());
+            eraseAlarm = new Intent();
+            Calendar calendar1 = new GregorianCalendar();
+            calendar1.setTimeInMillis(toKill);
+            eraseAlarm.setAction(ACTION_DISMISS_ALARM);
+            eraseAlarm.putExtra(AlarmClock.EXTRA_HOUR, calendar1.HOUR);
+            eraseAlarm.putExtra(AlarmClock.EXTRA_MINUTES, calendar1.MINUTE);
+            eraseAlarm.putExtra(AlarmClock.EXTRA_IS_PM, false);
+            eraseAlarm.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+            startActivity(eraseAlarm);
+        }
+        //put record
+        try {
+            wait(1000);
+        }
+        catch (Exception e){
+
+        }
         putRecord(rc);
+
+
     }
-    public static class CalEvent
-    {
-        String title;
-        Date start;
-
-        public CalEvent(String a, Date b)
-        {
-            title = a;
-            start = b;
-        }
-
-        public String toString()
-        {
-            return title + ": " + start;
-        }
-    }
-
-
-    private final LocationListener listener = new LocationListener() {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            // A new location update is received.  Do something useful with it.  In this case,
-            // we're sending the update to a handler which then updates the UI with the new
-            // location.
-            Message.obtain(mHandler,
-                    UPDATE_LATLNG,
-                    location.getLatitude() + ", " +
-                            location.getLongitude()).sendToTarget();
-        }
-    };
-
-    LocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000000, 100, listener);
-
 }
